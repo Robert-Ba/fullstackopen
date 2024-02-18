6d4c723b4ed4b23d6a78190bbcc6921aad61d016
Bob <rbalchunas13@gmail.com>
2024-02-18 11:38:36 -0500

    Part 2 exercise 2.11 Phonebook using json-server
    Get persons from json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
@@ -11,15 +11,17 @@ import Persons from './components/Persons'
  * @component
  */
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [nameFilter, setFilter] = useState('')
+
+  // Get persons from json-server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((res) => setPersons(res.data))
+  }, [])
 
   // Filter persons using regex.
   const personsFiltered = persons.filter(person => {
